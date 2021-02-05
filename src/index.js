a195b229f6e6257922a62917f374e8cf8475b0a4
SamSmith <sam@gsamsmith.com>
2021-02-04 15:51:44 -1000

    Changed file structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { Fragment, Component, useState, useEffect } from "@wordpress/element";
-import Tabs from './View/Tabs';
+import Tabs from './Components/Tabs';
 
 wp.element.render(<Tabs />, document.querySelector(`#react-template`));
